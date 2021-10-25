9b5af124d0b68cf15261bb0af30726268618bffa
Alexandr Burtsev <KapitanZerg@gmail.com>
2021-10-25 15:04:02 +0300

    Add checking to error

--- SongLyrics/SongLyrics/ViewControllers/ViewController.swift
+++ SongLyrics/SongLyrics/ViewControllers/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, APILyricsManagerDelegate {
     
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var lyricManager = APILyricsManager()
+    var tracksList = [TrackData]()
+    var track = TrackData(trackName: "", artistName: "", trackId: 0, hasLyric: 0, lyricBody: "")
+        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as? LyricViewController
+        vc?.track = track
+    }
+    //MARK: API Lyric Manager delegates
+    func updateTableData(_: APILyricsManager, with APIData: [TrackData]) {
+        tracksList = APIData
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
     func getLyric(_: APILyricsManager, with APIData: TrackData) {
         self.track = APIData
         
@@ -17,47 +36,59 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
-    
-    @IBOutlet weak var searchTextField: UITextField!
-    @IBOutlet weak var tableView: UITableView!
-    
-    var lyricManager = APILyricsManager()
-    var tracksList = [TrackData]()
-    var track = TrackData(trackName: "", artistName: "", trackId: 0, hasLyric: 0, lyricBody: "")
-    var lyric: String = "ghbd"
-    var trackID: Int = 0
-        
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let vc = segue.destination as? LyricViewController
-        vc?.track = track
-    }
-    
-    func updateTableData(_: APILyricsManager, with APIData: [TrackData]) {
-        tracksList = APIData
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-        }
-    }
-    
+    //MARK: Table view data source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tracksList.count
+        if searchTextField.text == "" || searchTextField.text == nil {
+            return 1
+        }
+        else {
+            if tracksList.count == 0 {
+                return 1
+            }
+            else {
+                return tracksList.count
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         tableView.allowsSelection = true
         let cell = tableView.dequeueReusableCell(withIdentifier: "SongItem", for: indexPath)
-        let songsList = tracksList[indexPath.row]
-        cell.textLabel!.text = songsList.trackName
-        cell.detailTextLabel!.text = songsList.artistName
-        if (songsList.hasLyric == 0) {
+        
+        if searchTextField.text == nil || searchTextField.text == "" {
+            cell.textLabel!.text = "Enter the words to search for"
+            cell.detailTextLabel!.text = .none
             cell.accessoryType = .none
+            tableView.allowsSelection = false
+//            cell.layer.borderColor = .none
+            return cell
         }
         else {
-            cell.accessoryType = .checkmark
+            if tracksList.count == 0 {
+                cell.textLabel!.text = "Nothing found!"
+                cell.detailTextLabel!.text = .none
+                cell.accessoryType = .none
+                tableView.allowsSelection = false
+//                cell.layer.borderColor = .none
+                return cell
+            }
+            else {
+                let songsList = tracksList[indexPath.row]
+                cell.textLabel!.text = songsList.trackName
+                cell.detailTextLabel!.text = songsList.artistName
+                if (songsList.hasLyric == 0) {
+                    cell.accessoryType = .none
+                }
+                else {
+                    cell.accessoryType = .checkmark
+                }
+                cell.selectionStyle = .default
+                return cell
+            }
         }
-        return cell
     }
     
+    //MARK: Table view delegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedCell = tracksList[indexPath.row]
         
@@ -72,7 +103,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    
     @IBAction func search(_ sender: Any) {
+        if let text = self.searchTextField.text {
+            lyricManager.searchInAPILibrary(withWords: text)
+        }
         tableView.reloadData()
     }
     
